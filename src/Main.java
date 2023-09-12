f07519bc2b320fcadfc0329090a4ce981953e639
unknown <heodaikadz2k3@gmail.com>
2023-09-12 09:15:52 +0700

    second commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+import controller.Letter_Char_CountPrograming;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author CongThanh
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Letter_Char_CountPrograming().run();
+    }
+}
